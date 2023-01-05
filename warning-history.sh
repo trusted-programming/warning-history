7c896abbf24add110b19f29f6d78bf2cfe5df73f
Yijun Yu <y.yu@open.ac.uk>
2023-01-05 22:34:46 +0000

    update

--- warning-history.sh
+++ warning-history.sh
@@ -3,6 +3,16 @@ hash rust-diagnostics
 if ! [ $? == 0 ]; then
 	cargo install rust-diagnostics
 fi
+if [ "$1" == "" ]; then
+	if [ -d .git ]; then
+		repo=.
+	else
+		echo Please provide the path to git repository as an argument
+	fi
+else
+	repo=$1
+fi
+pushd $repo > /dev/null
 git tag -f original
 n=0
 rm -rf diagnosticses
@@ -19,3 +29,4 @@ git log -p --reverse | grep "^commit " | cut -d" " -f2 | while read f; do
 	mv $n.txt diagnosticses/$n/counts.txt
 done
 git checkout -f original
+popd > /dev/null
