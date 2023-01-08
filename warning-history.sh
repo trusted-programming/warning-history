6e22b1d9c7a5fb2bfdb386e83f5cdd4857fd221c
Yijun Yu <y.yu@open.ac.uk>
2023-01-08 17:23:38 +0000

    print statistics of warnings

--- warning-history.sh
+++ warning-history.sh
@@ -24,12 +24,14 @@ pushd $repo > /dev/null
 if [ ! -d diagnosticses ]; then
 	n=0
 	mkdir -p diagnosticses
-	git log -p --reverse | grep "^commit " | cut -d" " -f2 | while read f; do
+	git log -p --reverse | grep "^commit " | cut -d" " -f2 > git.log
+	cat git.log | while read f; do
 		git stash
 		n=$(( n + 1 ))
 		git checkout $f
 		tokei -t=Rust src > $n-tokei.txt 
-		rust-diagnostics > $n.txt
+		g=$(grep -A1 $f git.log | tail -1)
+		rust-diagnostics --patch $g --confirm > $n.txt
 		if [ -d diagnostics ]; then
 			mkdir -p diagnosticses/$n/$f
 			mv diagnostics diagnosticses/$n/$f/
@@ -43,17 +45,19 @@ if [ ! -d diagnosticses ]; then
 	git checkout -f main
 fi
 #echo revision,warning,LOC,hash,date> counts.csv
-echo revision,date,warning,LOC> counts.csv
+echo date,warning,warning/file,warning/KLOC> counts.csv
 find diagnosticses -name "counts.txt" | while read f; do
-	g=$(dirname $(dirname $f)/*/diagnostics)
+	g=$(basename $(dirname $f)/[a-f0-9][a-f0-9][a-f0-9]*)
 	rev=$(basename $g)
 #	d=$(git log $rev --pretty=format:'%ad' --date=iso |head -1)
 	d=$(git log $rev --pretty=format:'%at' --date=iso |head -1)
 #	echo $(basename $(dirname $f)),$(cat $f | grep -v "previously generated" | awk '{print $3, ",", $6}'),$(cat ${f/counts/tokei} | grep " Total" | awk '{print $3}'),$(basename $g),$d
 #	echo $(basename $(dirname $f)),"\""$d"\"",$(cat $f | grep -v "previously generated" | awk '{print $3, ",", $6}'),$(cat ${f/counts/tokei} | grep " Total" | awk '{print $3}')
-echo $(( d - 1667370179 )),$(cat $f | grep -v "previously generated" | awk '{print $3, ",", $6}'),$(cat ${f/counts/tokei} | grep " Total" | awk '{print $3}')
+echo $(( d - 1667370179 )),$(cat $f | grep -v "previously generated" | head -1 | awk '{printf("%d,%d\n", $3, $6)}'),$(cat ${f/counts/tokei} | grep " Total" | awk '{print $3}')
 done | sort -n -k1 >> counts.csv
+grep "^\#\[Warning" diagnosticses/*/counts.txt | cut -d: -f2-4 | sort | uniq -c | sort -n
 #gnuplot -p $p/warning-history.gnuplot
 #gnuplot -p $p/warning-history-files.gnuplot
 gnuplot -p $p/warning-history-LOC.gnuplot
 popd > /dev/null
+
