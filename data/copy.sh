f10b725c2148d5da8f17cec5051c6fc9fade0d4a
Yijun Yu <y.yu@open.ac.uk>
2023-01-26 05:19:52 +0000

    update

--- data/copy.sh
+++ data/copy.sh
@@ -1,8 +1,8 @@
 #!/bin/bsh
 page=/var/www/html/rust-diagnostics2.html
 echo > $page
-#echo "<a href='../clippy-warning-fix-count.csv'>warning fixes counts</a>" >> $page
-#echo "<a href='../clippy-warning-fix.tar.bz2'>data</a><br/>" >> $page
+echo "<a href='../clippy-warning-fixes-count-function.csv'>warning fixes counts</a>" >> $page
+echo "<a href='../clippy-warning-fixes-function.tar.bz2'>data</a><br/>" >> $page
 echo "Ordered by length of git history <ul>" >> $page
 cat ../../../rust-lang/rust-counts.csv | while read a; do
    f=${a%% *}
